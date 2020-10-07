0a9549a721cdcb3885c538f86da9881f51bfae6f
Stephen Cocuzza <scocuzza3@gmail.com>
2020-10-07 14:00:39 -0400

    start new game and show Player's turn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 console.log("Script has been linked");
+
+
+init()
+
+
+function init() {
+    //add listener to 'New game button'
+    document.querySelector('.new-game-btn')
+    .addEventListener('click', createNewGame)
+}
+function createNewGame(event) {
+    event.preventDefault()
+    const board = document.querySelector('.board')
+    if (board.style.display === "") {
+        board.style.display = "block"
+    } else {
+        board.style.display = ""
+    }
+    console.log('A new game has been created');
+    document.querySelector('.turnDisplay').innerHTML = "Player 1's Turn"
+}
