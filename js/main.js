a8d119d1f9f181e73b7f9f2d95e57406111f9b2f
Stephen Cocuzza <scocuzza3@gmail.com>
2020-10-07 16:34:06 -0400

    Evaluate winner - in console.log

--- js/main.js
+++ js/main.js
@@ -39,11 +39,12 @@ function selectSquare(event) {
     if (!checkIfButtonSelected(event)) {
         event.target.innerHTML = currentPlayer.character
         currentPlayer.currentTurn = false;
-        event.target.value = "selected"
+        event.target.value = currentPlayer.character
+        checkForWinner()
         changePlayersTurn()
         displayPlayerTurn()
     } else {
-        alert("This Box was already chosen!! \nPlease Try Again")
+        alert("This Box was already chosen!! \nPlease Choose Another Box")
     }
 }
 
@@ -68,9 +69,74 @@ function displayPlayerTurn() {
 }
 
 function checkIfButtonSelected(event) {
-    if (event.target.value === "selected") {
+    console.log(event.target.value);
+    if (event.target.value == 'X' && event.target.value == 'O') {
         return true;
     } else {
         false;
     }
+}
+function checkForWinner() {
+    buttons = document.querySelectorAll('.button')
+    let valueArray = []
+    buttons.forEach( (button, index) => {
+       valueArray.push(button.value)
+    })
+    console.log(valueArray);
+    //======= horizontal matches
+    if(valueArray[0] === valueArray[1] && 
+        valueArray[1] === valueArray[2] && 
+        valueArray[0] === valueArray[2] ) {
+        displayWinner(valueArray[0])
+    } else if(valueArray[3] === valueArray[4] && 
+        valueArray[4] === valueArray[5] && 
+        valueArray[3] === valueArray[5] ) {
+        displayWinner(valueArray[3])
+    } else if(valueArray[6] === valueArray[7] && 
+        valueArray[8] === valueArray[9] && 
+        valueArray[6] === valueArray[9] ) {
+        displayWinner(valueArray[6])
+    //====== vertical matches
+    } else if(valueArray[0] === valueArray[3] && 
+        valueArray[3] === valueArray[6] && 
+        valueArray[0] === valueArray[6] ) {
+        displayWinner(valueArray[0])
+    } else if(valueArray[1] === valueArray[4] && 
+        valueArray[4] === valueArray[7] && 
+        valueArray[1] === valueArray[7] ) {
+        displayWinner(valueArray[1])
+    } else if(valueArray[2] === valueArray[5] && 
+        valueArray[5] === valueArray[8] && 
+        valueArray[2] === valueArray[8] ) {
+        displayWinner(valueArray[1])
+    }
+    //===== diagonal matches 
+    else if(valueArray[0] === valueArray[4] && 
+        valueArray[4] === valueArray[8] && 
+        valueArray[0] === valueArray[8] ) {
+        displayWinner(valueArray[0])
+    } else if(valueArray[2] === valueArray[4] && 
+        valueArray[4] === valueArray[6] && 
+        valueArray[2] === valueArray[6] ) {
+        displayWinner(valueArray[2])
+    }
+    //===== horizontal winners
+    // index 0, 1, 2 match - done
+    // index 3, 4, 5 match - done
+    // index 6, 7, 8 match - done
+    //===== vertical winners
+    // index 0, 3, 6 match - done
+    // index 1, 4, 7 match - done 
+    // index 2, 5, 8 match - done 
+    //===== diagonal winners
+    // index 0, 4, 8 match - done
+    // index 2, 4, 6 match - done
+}
+function displayWinner(winningCharacter) {
+    if(winningCharacter === player1.character) {
+        console.log(player1.name + " is the winner!");
+    } else if(winningCharacter === player2.character) {
+        console.log(player2.name + " is the winner!");
+    }
+
 }
